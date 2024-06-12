f88d1678fd9cbbce391c2275f3cd3dddf7a04a2f
parfitt <lee@lee.co.uk>
2024-06-12 15:28:42 +0100

    added update to run.sh file

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo "Hello from the Jenkins job named: ${JOB_NAME}"
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
 echo "this is txt file 1" > 1.txt
+echo "this is a change to txt 1 file" > 1.txt
 echo "this is txt file 2" > 2.txt
 echo "this is txt file 3" > 3.txt
 echo "this is txt file 4" > 4.txt
