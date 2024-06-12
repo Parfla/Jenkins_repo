aa6605402e21dda5c62884a50eb0abc8cd469499
parfitt <lee@lee.co.uk>
2024-06-12 15:20:18 +0100

    added sh file

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-echo 'Hello world!'
-exit 1
+echo "Hello from the Jenkins job named: ${JOB_NAME}"
+touch 1.txt 2.txt 3.txt 4.txt 5.txt
+echo "this is txt file 1" > 1.txt
+echo "this is txt file 2" > 2.txt
+echo "this is txt file 3" > 3.txt
+echo "this is txt file 4" > 4.txt
+echo "this is txt file 5" > 5.txt
+zip archive.zip *.txt
